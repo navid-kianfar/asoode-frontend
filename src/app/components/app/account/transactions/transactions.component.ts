453dff38f491a06fa1bb116e49c80e3102675a8b
Navid Kianfar <nvd.kianfar@gmail.com>
2020-02-26 16:38:34 +0330

    header

--- src/app/components/app/account/transactions/transactions.component.ts
+++ src/app/components/app/account/transactions/transactions.component.ts
@@ -16,7 +16,7 @@ export class TransactionsComponent implements OnInit {
     'previousDebt',
     'download',
   ];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<any>([]);
 
   constructor(private readonly translatorService: MaterialTranslatorService) {}
 
@@ -27,83 +27,3 @@ export class TransactionsComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
   }
 }
-export interface PeriodicElement {
-  title: string;
-  amount: number;
-  createdAt: Date;
-  dueAt: Date;
-  previousDebt: number;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 100,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 200,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 300,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 400,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 500,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 600,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 700,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 800,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 900,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-  {
-    title: 'فاکتور اردیبهشت ماه',
-    amount: 1000,
-    dueAt: new Date(),
-    createdAt: new Date(),
-    previousDebt: 0,
-  },
-];
