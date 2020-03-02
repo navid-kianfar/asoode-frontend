22dc0509276c7ad4a159b99e544018af1e0adb93
Navid Kianfar <nvd.kianfar@gmail.com>
2020-03-02 21:05:14 +0330

    before buttons added

--- src/app/pages/storage/files/files.component.ts
+++ src/app/pages/storage/files/files.component.ts
@@ -9,11 +9,18 @@ import {ProjectService} from '../../../services/projects/project.service';
   styleUrls: ['./files.component.scss'],
 })
 export class FilesComponent implements OnInit {
+  sharedAnyThing: boolean;
+  detail: boolean;
   constructor(
     private readonly messengerService: MessengerService,
     private readonly groupService: GroupService,
     private readonly projectService: ProjectService,
   ) {
   }
-  ngOnInit() { }
+  ngOnInit() {
+    this.sharedAnyThing =
+      this.messengerService.channels.length > 0 ||
+      this.projectService.projects.length > 0 ||
+      this.groupService.groups.length > 0 || false;
+  }
 }
