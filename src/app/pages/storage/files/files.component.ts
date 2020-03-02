13e76383a396f245724822702f8060e5782adbae
Navid Kianfar <nvd.kianfar@gmail.com>
2020-03-02 20:40:14 +0330

    files step 2

--- src/app/pages/storage/files/files.component.ts
+++ src/app/pages/storage/files/files.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MessengerService} from '../../../services/communication/messenger.service';
+import {GroupService} from '../../../services/groups/group.service';
+import {ProjectService} from '../../../services/projects/project.service';
 
 @Component({
   selector: 'app-files',
@@ -6,5 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./files.component.scss'],
 })
 export class FilesComponent implements OnInit {
+  constructor(
+    private readonly messengerService: MessengerService,
+    private readonly groupService: GroupService,
+    private readonly projectService: ProjectService,
+  ) {
+  }
   ngOnInit() { }
 }
