08311e98d9631614791009e6f4d5fdebc57f8f21
Navid Kianfar <nvd.kianfar@gmail.com>
2020-03-10 22:19:26 +0330

    org chart #3

--- src/app/components/app/org-chart/org-chart.component.ts
+++ src/app/components/app/org-chart/org-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {GroupViewModel} from '../../../view-models/groups/group-types';
 
 @Component({
   selector: 'app-org-chart',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./org-chart.component.scss'],
 })
 export class OrgChartComponent implements OnInit {
+  @Input() group: GroupViewModel;
+
   constructor() {}
 
   ngOnInit() {}
