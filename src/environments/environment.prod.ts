c27a0cf919537a10644b286703fca868e1dde631
Navid Kianfar <nvd.kianfar@gmail.com>
2020-04-04 05:16:56 +0430

    ?!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  socket_endpoint: '',
+  socket_endpoint: 'https://socket.asoode.com',
   api_endpoint: 'https://api.asoode.com/v2',
   googleOauth: 'https://api.asoode.com/oauth/google-login',
 };
