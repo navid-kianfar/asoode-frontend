0be6bc6fe26d6a0b8119d309e6110d2e2b8bbd3b
Navid Kianfar <nvd.kianfar@gmail.com>
2020-11-23 15:19:06 +0330

    multiple environments

---
+++ src/environments/environment.alrahah.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  socket_disabled: false,
+  socket_endpoint: 'https://socket.al-rahah.com',
+  api_endpoint: 'https://api.al-rahah.com/v2',
+  direct_endpoint: 'https://api.al-rahah.com/v2',
+  googleOauth: 'https://api.al-rahah.com/oauth/google-login',
+  vapid:
+    'BP6o2qiosTeFgjZ4maMyfgZb33ic3lZFKeCqtFzO1BMfW2e9Qieq0HYbya8z-tr6Olza10cW_nYPiGWO3umvEIY',
+};
---
+++ src/environments/environment.ciblsoft.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  socket_disabled: false,
+  socket_endpoint: 'https://socket.ciblsoft.com',
+  api_endpoint: 'https://api.ciblsoft.com/v2',
+  direct_endpoint: 'https://api.ciblsoft.com/v2',
+  googleOauth: 'https://api.ciblsoft.com/oauth/google-login',
+  vapid:
+    'BP6o2qiosTeFgjZ4maMyfgZb33ic3lZFKeCqtFzO1BMfW2e9Qieq0HYbya8z-tr6Olza10cW_nYPiGWO3umvEIY',
+};
